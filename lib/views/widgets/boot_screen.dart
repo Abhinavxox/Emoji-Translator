a357664481502c42c3b860cbf26bf2d5f72ba81a
Abhinavxox <abhinavxox@gmail.com>
2022-12-13 19:53:43 +0530

    <addition>:[created the bootup page]

---
+++ lib/views/widgets/boot_screen.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:emoji_translator/views/home.dart';
+
+class BootScreen extends StatefulWidget {
+  @override
+  State<BootScreen> createState() => _BootScreenState();
+}
+
+class _BootScreenState extends State<BootScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Color(0xFFBD6E4E5),
+      child:  Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Container(
+              child: Column(
+                  children: <Widget>[
+                    Container(
+                      child: Container(
+                        child: Text("EMOJI TRANSLATOR",
+                          style: TextStyle(
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      child: Center(
+                        child: Image.asset('assets/images/box.png'),
+                      ),
+                    ),
+
+                  ]
+              )
+          ),
+
+          Container(
+            child: Container(
+              margin: EdgeInsets.only(top: 100),
+              padding: EdgeInsets.all(10),
+              width: 200,
+              decoration: BoxDecoration(
+                color: Color(0xFFBEB6440),
+                borderRadius: BorderRadius.circular(50),
+              ),
+              child: TextButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => Homepage()),
+                  );
+                },
+                child: Text("Get Started",
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
